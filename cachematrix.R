087125aaaa603e1e3732ff45ce8222887b3166d8
John Power <jp11235@gmail.com>
2015-07-25 19:13:33 -0500

    adding correct code with a comments at end

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,6 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -10,25 +9,45 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getmean()
+  m <- x$getinverse()
   if(!is.null(m)) {
-    message("getting cached data")
+    message("getting cached inverse")
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...)
+  x$setinverse(m)
   m
 }
+
+#run prog assign #2 at the console
+#> ls()
+#[1] "cacheSolve"      "makeCacheMatrix"
+#> a <- makeCacheMatrix(matrix(c(1,0,5,2,1,6,3,4,0),nrow = 3,ncol = 3))
+#> a$get()
+#     [,1] [,2] [,3]
+#[1,]    1    2    3
+#[2,]    0    1    4
+#[3,]    5    6    0
+#> cacheSolve(a)
+#     [,1] [,2] [,3]
+#[1,]  -24   18    5
+#[2,]   20  -15   -4
+#[3,]   -5    4    1
+#> cacheSolve(a)
+#getting cached inverse
+#     [,1] [,2] [,3]
+#[1,]  -24   18    5
+#[2,]   20  -15   -4
+#[3,]   -5    4    1
